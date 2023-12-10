57ec923f107bef2ddc0f20aa91bf231c73032372
DewiRosanti <48055782+DewiRosanti@users.noreply.github.com>
2023-12-10 07:51:11 +0700

    Add files via upload

---
+++ game_online.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+int main()
+{
+    int harga = 10000,jam_main,bayar;
+    float diskon,total_bayar,total_diskon;
+    printf("Jam Main = "); scanf("%d",&jam_main);
+    if(jam_main <= 4){
+        diskon = 0;
+    } else if(jam_main > 4 && jam_main <= 6){
+        diskon = 10;
+    } else if(jam_main > 6 && jam_main <= 8){
+        diskon = 15;
+    } else if(jam_main > 8){
+        diskon = 20;
+    }
+
+    bayar = harga * jam_main;
+    total_diskon = (float)bayar * (diskon / 100);
+    total_bayar = (float)bayar - total_diskon;
+    printf("============================\n");
+    printf("Bayar = %d\n", bayar);
+    printf("Diskon = %.2f\n", total_diskon);
+    printf("Total Bayar = %.2f\n", total_bayar);
+
+    return 0;
+
+}
